009fdc80e4540e39d299f82bc5037142d363f430
vladimir.kruglov <vladimir.kruglov@arvato.com>
2016-11-15 09:48:52 +0200

    update

---
+++ UnitTestProject1/AI/BotCleanLargeTests.cs
@@ -0,0 +1,211 @@
+﻿using hak.AI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hak.AI.Tests
+{
+    [TestClass()]
+    public class BotCleanLargeTests
+    {
+        [TestMethod()]
+        public void next_moveTest()
+        {
+            var board = new string[]
+            {
+                "b---d",
+                "-d--d",
+                "--dd-",
+                "--d--",
+                "----d",
+            };
+            Assert.AreEqual("RIGHT", BotCleanLarge.next_move(0, 0, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest2()
+        {
+            var board = new string[]
+            {
+                "-b--d",
+                "-d--d",
+                "--dd-",
+                "--d--",
+                "----d",
+            };
+            Assert.AreEqual("DOWN", BotCleanLarge.next_move(0, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest3()
+        {
+            var board = new string[]
+            {
+                "----d",
+                "-d---", //-b---
+                "--dd-",
+                "--d--",
+                "----d",
+            };
+            Assert.AreEqual("RIGHT", BotCleanLarge.next_move(1, 0, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest31()
+        {
+            var board = new string[]
+            {
+                "----d",
+                "-b---",
+                "--dd-",
+                "--d--",
+                "----d",
+            };
+            Assert.AreEqual("RIGHT", BotCleanLarge.next_move(1, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest312()
+        {
+            var board = new string[]
+            {
+                "-b--d",
+                "-d---",
+                "--dd-",
+                "--d--",
+                "----d",
+            };
+            Assert.AreEqual("UP", BotCleanLarge.next_move(2, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest4()
+        {
+            var board = new string[]
+            {
+                "-----",
+                "-----", //-b---
+                "d--d-",
+                "---d-",
+                "--d-d",
+            };
+            Assert.AreEqual("LEFT", BotCleanLarge.next_move(1, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest42()
+        {
+            var board = new string[]
+            {
+                "-----",
+                "-----",
+                "---d-",
+                "---d-",
+                "--d-d",
+            };
+
+            Assert.AreEqual("RIGHT", BotCleanLarge.next_move(1, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest43()
+        {
+            var board = new string[]
+            {
+                "-----",
+                "-----",
+                "d--d-",
+                "---d-",
+                "--d-d",
+            };
+            Assert.AreEqual("LEFT", BotCleanLarge.next_move(1, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest44()
+        {
+            var board = new string[]
+            {
+                "-----",
+                "-----",
+                "d--d-",
+                "---d-",
+                "--d-d",
+            };
+            Assert.AreEqual("DOWN", BotCleanLarge.next_move(1, 0, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest41()
+        {
+            var board = new string[]
+            {
+                "-d---",
+                "-----",
+                "d--d-",
+                "---d-",
+                "--d-d",
+            };
+            Assert.AreEqual("LEFT", BotCleanLarge.next_move(2, 1, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest5()
+        {
+            var board = new string[]
+            {
+                "-d---",
+                "-d---",
+                "-----",
+                "---d-",
+                "--d-d",
+            };
+            Assert.AreEqual("DOWN", BotCleanLarge.next_move(2, 3, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest6()
+        {
+            var board = new string[]
+            {
+                "-----",
+                "-----",
+                "-----",
+                "-----",
+                "--d-d",
+            };
+            Assert.AreEqual("LEFT", BotCleanLarge.next_move(3, 3, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest61()
+        {
+            var board = new string[]
+            {
+                "-----",
+                "-----",
+                "-----",
+                "---d-",
+                "--d-d",
+            };
+            Assert.AreEqual("RIGHT", BotCleanLarge.next_move(3, 2, 5, 5, board));
+        }
+
+        [TestMethod()]
+        public void next_moveTest7()
+        {
+            var board = new string[]
+            {
+                "-d---",
+                "-d---",
+                "---d-",
+                "---d-",
+                "--d-d", //--b--
+            };
+            Assert.AreEqual("UP", BotCleanLarge.next_move(4, 3, 5, 5, board));
+        }
+    }
+}
---
+++ hak/AI/BotCleanStochastic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hak.AI
+{
+    public class BotCleanStochastic
+    {
+        public static string next_move(int posr, int posc, String[] board)
+        {
+            List<Tuple<int, int>> points = new List<Tuple<int, int>>();
+            for (int i = 0; i < board.Length; i++)
+            {
+                var line = board[i];
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == 'd')
+                    {
+                        if (j == posc && posr == i)
+                        {
+                            return "CLEAN";
+                        }
+                        points.Add(new Tuple<int, int>(i, j));
+                    }
+                }
+            }
+            var differences = new List<int>();
+            foreach (var point in points)
+            {
+                differences.Add(Math.Abs(posr - point.Item1) + Math.Abs(posc - point.Item2));
+            }
+            var min = board.Length + board.Length;
+            var minIndex = differences.Count;
+            for (int i = 0; i < differences.Count; i++)
+            {
+                if (differences[i] < min)
+                {
+                    min = differences[i];
+                    minIndex = i;
+                }
+            }
+            var minpoint = points[minIndex];
+            if (posc > minpoint.Item2)
+            {
+                return "LEFT";
+            }
+            if (posc < minpoint.Item2)
+            {
+                return "RIGHT";
+            }
+            if (posr < minpoint.Item1)
+            {
+                return "DOWN";
+            }
+
+            return "UP";
+        }
+    }
+}
